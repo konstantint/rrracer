baa5589984dbfeee50c82004653b0921f24cbed8
Konstantin Tretyakov <kt@ut.ee>
2013-03-24 07:33:13 +0200

    New version

--- bin/js/common.js
+++ bin/js/common.js
@@ -29,6 +29,28 @@ var Dom = {
 
 }
 
+var GameUtil = {
+	 /**
+	  * Load multiple images and callback when ALL images have loaded.
+	  */
+	loadImages: function (directory, names, callback) {
+		var result = [];
+		var count  = names.length;
+
+		var onload = function() {
+			if (--count == 0)
+				callback(result);
+		};
+
+		for(var n = 0 ; n < names.length ; n++) {
+			var name = names[n];
+			result[n] = document.createElement('img');
+			Dom.on(result[n], 'load', onload);
+			result[n].src = directory + "/" + name + ".png";
+		}
+	}
+}
+
 //=========================================================================
 // general purpose helpers (mostly math)
 //=========================================================================
@@ -79,142 +101,7 @@ var Util = {
 
 }
 
-//=========================================================================
-// POLYFILL for requestAnimationFrame
-//=========================================================================
 
-if (!window.requestAnimationFrame) { // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-  window.requestAnimationFrame = window.webkitRequestAnimationFrame || 
-                                 window.mozRequestAnimationFrame    || 
-                                 window.oRequestAnimationFrame      || 
-                                 window.msRequestAnimationFrame     || 
-                                 function(callback, element) {
-                                   window.setTimeout(callback, 1000 / 60);
-                                 }
-}
-
-//=========================================================================
-// GAME LOOP helpers
-//=========================================================================
-
-var Game = {  // a modified version of the game loop from my previous boulderdash game - see http://codeincomplete.com/posts/2011/10/25/javascript_boulderdash/#gameloop
-
-  run: function(options) {
-
-    Game.loadImages(options.images, function(images) {
-      options.ready(images); // tell caller to initialize itself because images are loaded and we're ready to rumble
-
-      Game.setKeyListener(options.keys);
-
-      var canvas = options.canvas,    // canvas render target is provided by caller
-          update = options.update,    // method to update game logic is provided by caller
-          render = options.render,    // method to render the game is provided by caller
-          step   = options.step,      // fixed frame step (1/fps) is specified by caller
-          stats  = options.stats,     // stats instance is provided by caller
-          now    = null,
-          last   = Util.timestamp(),
-          dt     = 0,
-          gdt    = 0;
-
-      function frame() {
-        now = Util.timestamp();
-        dt  = Math.min(1, (now - last) / 1000); // using requestAnimationFrame have to be able to handle large delta's caused when it 'hibernates' in a background or non-visible tab
-        gdt = gdt + dt;
-        while (gdt > step) {
-          gdt = gdt - step;
-          update(step);
-        }
-        render();
-        stats.update();
-        last = now;
-        requestAnimationFrame(frame, canvas);
-      }
-      frame(); // lets get this party started
-    });
-  },
-
-  //---------------------------------------------------------------------------
-
-  loadImages: function(names, callback) { // load multiple images and callback when ALL images have loaded
-    var result = [];
-    var count  = names.length;
-
-    var onload = function() {
-      if (--count == 0)
-        callback(result);
-    };
-
-    for(var n = 0 ; n < names.length ; n++) {
-      var name = names[n];
-      result[n] = document.createElement('img');
-      Dom.on(result[n], 'load', onload);
-      result[n].src = "images_tll/" + name + ".png";
-    }
-  },
-
-  //---------------------------------------------------------------------------
-
-  setKeyListener: function(keys) {
-    var onkey = function(keyCode, mode) {
-      var n, k;
-      for(n = 0 ; n < keys.length ; n++) {
-        k = keys[n];
-        k.mode = k.mode || 'up';
-        if ((k.key == keyCode) || (k.keys && (k.keys.indexOf(keyCode) >= 0))) {
-          if (k.mode == mode) {
-            k.action.call();
-          }
-        }
-      }
-    };
-    Dom.on(document, 'keydown', function(ev) { onkey(ev.keyCode, 'down'); } );
-    Dom.on(document, 'keyup',   function(ev) { onkey(ev.keyCode, 'up');   } );
-  },
-
-  //---------------------------------------------------------------------------
-
-  stats: function(parentId, id) { // construct mr.doobs FPS counter - along with friendly good/bad/ok message box
-
-    var result = new Stats();
-    result.domElement.id = id || 'stats';
-    Dom.get(parentId).appendChild(result.domElement);
-
-    var msg = document.createElement('div');
-    msg.style.cssText = "border: 2px solid gray; padding: 5px; margin-top: 5px; text-align: left; font-size: 1.15em; text-align: right;";
-    msg.innerHTML = "Your canvas performance is ";
-    Dom.get(parentId).appendChild(msg);
-
-    var value = document.createElement('span');
-    value.innerHTML = "...";
-    msg.appendChild(value);
-
-    setInterval(function() {
-      var fps   = result.current();
-      var ok    = (fps > 50) ? 'good'  : (fps < 30) ? 'bad' : 'ok';
-      var color = (fps > 50) ? 'green' : (fps < 30) ? 'red' : 'gray';
-      value.innerHTML       = ok;
-      value.style.color     = color;
-      msg.style.borderColor = color;
-    }, 5000);
-    return result;
-  },
-
-  //---------------------------------------------------------------------------
-
-  playMusic: function() {
-    var music = Dom.get('music');
-    music.loop = true;
-    music.volume = 0.05; // shhhh! annoying music!
-    music.muted = (Dom.storage.muted === "true");
-    music.play();
-    Dom.toggleClassName('mute', 'on', music.muted);
-    Dom.on('mute', 'click', function() {
-      Dom.storage.muted = music.muted = !music.muted;
-      Dom.toggleClassName('mute', 'on', music.muted);
-    });
-  }
-
-}
 
 //=========================================================================
 // canvas rendering helpers
@@ -334,7 +221,7 @@ var Render = {
   rumbleWidth:     function(projectedRoadWidth, lanes) { return projectedRoadWidth/Math.max(6,  2*lanes); },
   laneMarkerWidth: function(projectedRoadWidth, lanes) { return projectedRoadWidth/Math.max(32, 8*lanes); },
 
-  pitchMeter: function(ctx, normalizedSpeed, pitch, minPitch, maxPitch, maxPitchError) {
+  pitchMeter: function(ctx, normalizedSpeed, pitch, minPitch, maxPitch, maxPitchError, width, height) {
 	// Draw speedometer bar
     ctx.beginPath()
     var minAngle = 3*Math.PI/2-0.5;
--- bin/js/game.js
+++ bin/js/game.js
@@ -1,1 +1,56 @@
+//=========================================================================
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+//=========================================================================
+if (!window.requestAnimationFrame) { 
+  window.requestAnimationFrame = window.webkitRequestAnimationFrame || 
+                                 window.mozRequestAnimationFrame    || 
+                                 window.oRequestAnimationFrame      || 
+                                 window.msRequestAnimationFrame     || 
+                                 function(callback, element) {
+                                   window.setTimeout(callback, 1000 / 60);
+                                 }
+}
 
+/**
+ * Scene-based game looping class.
+ * There should only be one instance per document.
+ */
+function GameDirector(canvas, fps) {
+	var self = this;
+	this.canvas = canvas || Dom.get('canvas');
+	this.ctx  = this.canvas.getContext('2d');
+	this.fps = fps || 60;
+	
+	this.switchToScene = function(newScene) {
+		if (this.scene) this.scene.exit(this);
+		this.scene = newScene;
+		if (this.scene) this.scene.enter(this);
+	}
+	
+	this.run = function(scene) {
+		var now = Util.timestamp();
+		var last = Util.timestamp();
+		var step = 1/this.fps;
+		var dt = 0;
+		var gdt = 0;
+		
+		this.switchToScene(scene);
+		
+		function animationLoop() {
+			now = Util.timestamp();
+			dt  = Math.min(1, (now - last) / 1000);
+			gdt = gdt + dt;
+			while (gdt > step) {
+			  gdt = gdt - step;
+			  if (self.scene) self.scene.update(step);
+			}
+			if (self.scene) self.scene.render(self.ctx);
+			last = now;
+			requestAnimationFrame(animationLoop, self.canvas);
+		}
+		animationLoop();
+	}
+
+    Dom.on(document, 'keydown', function(ev) { if (self.scene) scene.keyDown(ev.keyCode); } );
+    Dom.on(document, 'keyup',   function(ev) { if (self.scene) scene.keyUp(ev.keyCode);   } );	
+}
